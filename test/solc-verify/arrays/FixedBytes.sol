a8a90b0e8fbe3725e95083bce360d465c882ee5f
Akos Hajdu <hajdua@mit.bme.hu>
2021-01-08 15:55:39 +0100

    Fix deprecated alias "byte"

--- test/solc-verify/arrays/FixedBytes.sol
+++ test/solc-verify/arrays/FixedBytes.sol
@@ -5,18 +5,18 @@ contract FixedBytes {
     bytes32 arr;
 
     // Safe, we check that index is in bounds
-    function getValueSafe(uint i) public view returns (byte) {
+    function getValueSafe(uint i) public view returns (bytes1) {
         if (i < arr.length) return arr[i];
         return 0;
     }
 
     // Safe, we check that index is in bounds
-    function getValueUnsafe(uint i) public view returns (byte) {
+    function getValueUnsafe(uint i) public view returns (bytes1) {
         return arr[i];
     }
 
     // Unsafe we don't know i
-    function singleByte(bytes1 param, uint i) public pure returns (byte) {
+    function singleByte(bytes1 param, uint i) public pure returns (bytes1) {
         assert(param.length == 1);
         return param[i];
     }
