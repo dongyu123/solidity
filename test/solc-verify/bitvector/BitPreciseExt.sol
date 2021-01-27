7dd3b4e0e1ae5d923ca67649d114f9baf38d4389
Dejan Jovanovic <dejan.jovanovic@sri.com>
2021-01-27 10:22:25 -0500

    fix test

--- test/solc-verify/bitvector/BitPreciseExt.sol
+++ test/solc-verify/bitvector/BitPreciseExt.sol
@@ -4,12 +4,13 @@ pragma solidity >=0.7.0;
 contract BitPreciseExt {
     function u32tou40(uint32 x) private pure returns (uint40) { return x; }
     function s16tos48(int16 x) private pure returns (int48) { return x; }
-    function u32tos40(uint32 x) private pure returns (int40)  { return x; }
+    // disallowed in 0.8.1
+    // function u32tos40(uint32 x) private pure returns (int40)  { return int40(x); }
 
     receive() external payable {
         assert(u32tou40(123) == 123);
         assert(s16tos48(123) == 123);
         assert(s16tos48(-123) == -123);
-        assert(u32tos40(123) == 123);
+        // assert(u32tos40(123) == 123);
     }
 }
